537abdff2239f45d77b638d6564d819115db118a
heestory-e <heestory@ex-em.com>
2022-10-19 23:43:08 +0900

    [섹션3] 새로운 할인 정책 적용과 문제점
    - 인터페이스에만 의존하도록 OrderServiceImpl 코드를 수정했다.
    - DiscountPolicy 의 구현체가 없으므로 NullPointerException 이 발생한다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +8,8 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 인터페이스에만 의존한다. (DIP)
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
