a6cb2d7a5149a0be29fc0e2822351b5b02f3213b
heestory-e <heestory@ex-em.com>
2022-10-20 23:23:26 +0900

    [섹션3] 관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,12 @@
 package hello.core.member;
 
-// 구현체가 하나만 있을 때는 인터페이스Impl 형태로 만들어 사용한다 (관례)
 public class MemberServiceImpl implements MemberService {
 
-    // 인터페이스 의존, 구현체 의존 DI 를 잘 지키고 있지 못하다 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
