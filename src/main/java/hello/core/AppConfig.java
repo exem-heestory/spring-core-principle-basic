31d88564e95e69176015b7ef03f42e6ae3a21b49
heestory-e <heestory@ex-em.com>
2022-10-26 00:08:07 +0900

    [섹션3] 새로운 구조와 할인 정책 적용
    as-is: FixDiscountPolicy (정액 할인 정책)
    to-be: RateDiscountPolicy (정률 할인 정책)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,7 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -23,6 +23,6 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
