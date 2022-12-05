ab927eed7569941224ea4c4f5a82bd8e4e75ec60
heestory-e <heestory@ex-em.com>
2022-12-05 23:14:42 +0900

    [섹션 7] 의존관계 자동 주입 - @Autowired 매칭
    - 1. 타입 매칭
    - 2. 타입 매칭의 결과가 2개 이상일 때 필드 명, 파라미터 명으로 빈 이름 매칭 (여기서는  rateDiscountPolicy)

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,8 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
-public class FixDiscountPolicy implements DiscountPolicy{
+@Component
+public class FixDiscountPolicy implements DiscountPolicy {
 
     private final int discountFixAmount = 1000; // 1000원 할인
 
