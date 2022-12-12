a06a6c51654e2396a7fc7d0af0aad976f1a577d5
heestory-e <heestory@ex-em.com>
2022-12-13 01:44:56 +0900

    [섹션 9] 빈 스코프 - 프로토타입 스코프 (JSR-330 Provider)

--- src/test/java/hello/core/scope/PrototypeProviderTest.java
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -1,13 +1,13 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,11 +30,10 @@ public class PrototypeProviderTest {
     static class ClientBean {
 
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
-//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
